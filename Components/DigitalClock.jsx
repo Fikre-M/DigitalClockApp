8f2a23f0b04f3c98fdfd06285a2671c4924d9086
Fikre-M <fikreddu@gmail.com>
2025-06-05 21:47:18 -0400

    add date, month and year

--- Components/DigitalClock.jsx
+++ Components/DigitalClock.jsx
@@ -1,43 +1,73 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from "react";
 
 function DigitalClock() {
-
     const [time, setTime] = useState(new Date());
 
     useEffect(() => {
         const intervalId = setInterval(() => {
         setTime(new Date());
-        }, 1000)
+        }, 1000);
 
-    return () => {
-        clearInterval(intervalId);
-    };
-    }, []); 
-    
-    function formatTime(){
+        return () => clearInterval(intervalId);
+    }, []);
+
+    function formatTime() {
         let hours = time.getHours();
         const minutes = time.getMinutes();
         const seconds = time.getSeconds();
         const meridiem = hours >= 12 ? "PM" : "AM";
-
         hours = hours % 12 || 12;
-
-        return `${padZero(hours)}:${padZero(
-            minutes
-        )}:${padZero(seconds)} ${meridiem}`;
+        return `${padZero(hours)}:${padZero(minutes)}:${padZero(
+        seconds
+        )} ${meridiem}`;
     }
-    // if one of the digit is sigle make double 00
-    function padZero(number){
+
+    function formatDate() {
+        const days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        ];
+        const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+        ];
+        const dayName = days[time.getDay()];
+        const monthName = months[time.getMonth()];
+        const date = time.getDate();
+        const year = time.getFullYear();
+        return `${dayName}, ${monthName} ${padZero(date)}, ${year}`;
+    }
+
+    function padZero(number) {
         return (number < 10 ? "0" : "") + number;
     }
 
     return (
-    <div className='clock-container'>
-        <div className='clock'>
+        <div className="clock-container">
+            <h1>Digital Clock App</h1>
+        <div className="clock">
             <span>{formatTime()}</span>
         </div>
-    </div>
-    )
+        <div className="date">
+            <span>{formatDate()}</span>
+        </div>
+        </div>
+    );
 }
 
-export default DigitalClock
+export default DigitalClock;
